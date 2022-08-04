63a3380de9466639824fe768782b0b8c3a1170e8
Samuel Amagbakhen <frontend_dev1@academystack.com>
2022-08-04 23:51:12 +0100

    WIP: Almost with the application

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors:{
+      'primary': 'rgb(85, 123, 231)',
+      'white': '#fff',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
